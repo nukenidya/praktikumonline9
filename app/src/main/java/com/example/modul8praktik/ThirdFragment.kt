3631dc735f4b3a9ce88accaba1a15b972494b63b
nukenidya <nidyanuke12@gmail.com>
2020-04-02 16:37:33 +0700

    Praktik+LatihanModul8

---
+++ app/src/main/java/com/example/modul8praktik/ThirdFragment.kt
@@ -0,0 +1,30 @@
+package com.example.modul8praktik
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+
+class ThirdFragment : Fragment() {
+    private var communicationViewModel: CommunicationViewModel? = null
+    private var txtName: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        communicationViewModel = ViewModelProviders.of(requireActivity()).get(CommunicationViewModel::class.java)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_third, container, false)
+    }
+
+    companion object {
+        fun newInstance(): ThirdFragment{
+            return ThirdFragment()
+        }
+    }
+}
